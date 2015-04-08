100fc940bcfecc4ee39d038258947067340b9cf1
bassoGeorge <anishgeorgehb@gmail.com>
2015-04-08 12:00:01 +0530

    Added OperationStatus and minor changes to the Job hierarchy. Also added MessageToClient type and the Log message

--- app/models/batch/job/package.scala
+++ app/models/batch/job/package.scala
@@ -5,10 +5,10 @@ import play.api.data._
 /**
  * Created by basso on 07/04/15.
  *
- * This package contains the Job hierarchy. This is used to identify individual jobs in the
- * batch.
- *
- * The package also contains the required mapping for the user input form to job. (handled by the job factory)
+ * This package contains:
+ *  1. The Job class hierarchy. This is used to identify individual jobs in the batch
+ *  2. The Mapping for the user input from to Job. (Handled by the Job factory)
+ *  3. The Job Factory object
  */
 package object job {
   val jobListForm = Form(
--- app/models/batch/package.scala
+++ app/models/batch/package.scala
@@ -10,10 +10,20 @@ package models
  *  1. The definition of the Batch class
  *  2. The sub-package job which contains the Job hierarchy
  *  3. The Batch factory which will create the actual Batch object
+ *  4. The OperationStatus enumeration
  *
  * Maintained by Anish
  */
 package object batch {
+
+  /** OperationStatus
+    * A Job or Batch may finish execution with one of the following status.
+    * This is needed for logging perposes
+    */
+  object OperationStatus extends Enumeration {
+    type OperationStatus = Value
+    val OpSuccess, OpFailure, OpWarning = Value
+  }
 }
 
 // TODO: Create the batch class
