59e57994be5257ff91785e3e57bf6408578ba077
Chetan Bhasin <git@mail.chetanbhasin.com>
2015-04-09 17:39:32 +0530

    Clean up again

---
+++ app/models/mining/mlops/Clustering.scala
@@ -0,0 +1,35 @@
+package models.mining.mlops
+
+import org.apache.spark.mllib.clustering._
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd.RDD
+
+/**
+ * Created by chetan on 12/03/15.
+ */
+
+/**
+ * models.mining.oldAlgo.Clustering using KMeans algorithm
+ * @param numClusters Number of clusters required
+ * @param numIterations Number of iterations required (defaults to 20)
+ */
+class VClustering(file: String, numClusters: Int, numIterations: Int = 20) {
+
+  lazy val data: (RDD[String]) = sc.textFile(file)
+
+  private lazy val supply = data.map(Vectors.parse(_))
+  lazy val clusters = KMeans.train(supply, numClusters, numIterations)
+
+  def run = println("models.mining.mlops.Clustering:\n" + clusters.toString)
+
+  def saveToTextFile(fileLocation: String) = {
+    val dataset = clusters.predict(supply) zip supply
+    dataset.saveAsTextFile(fileLocation)
+  }
+
+  def saveToObject(fileLocation: String) = {
+    val dataset = clusters.predict(supply) zip supply
+    dataset.saveAsObjectFile(fileLocation)
+  }
+
+}
