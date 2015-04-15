4034cabbf6b83b395c4e3e07073e7b373d852e7e
Chetan Bhasin <git@mail.chetanbhasin.com>
2015-04-15 23:29:15 +0530

    Finished with doubts

--- app/actors/persistenceManager/WorkerActor.scala
+++ app/actors/persistenceManager/WorkerActor.scala
@@ -1,27 +1,81 @@
 package actors.persistenceManager
 
-import akka.actor.Actor
+import java.io.File
+import java.net.URL
+import java.nio.file.{Files, Paths}
+
+import akka.actor.{Actor, ActorRef}
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.concurrent.Await
 import models.batch.job._
-import models.messages.batchProcessing.SubmitDsOpJob
+import models.messages.batchProcessing._
+import models.messages.persistenceManaging.datasetEntry
+
+import scala.sys.process._
 
 
 /**
  * Created by chetan on 13/04/15.
  */
-class WorkerActor extends Actor {
+class WorkerActor(mediator: ActorRef) extends Actor {
 
-  private def handleDsJob(username: String, job: DataSetOp) = job match {
+  private implicit val timeout = Timeout(10 seconds)
 
-    case DsAddFromUrl(name, desc, target_algo, url, id) => ???
-    case DsAddDirect(name, desc, target_algo, file, id) => ???
-    case DsDelete(name, id) => ???
-    case DsRefresh(name, id) => ???
+  private def handleDsJob(username: String, job: DataSetOp, dsm: ActorRef) = job match {
+
+    /**
+     * Download a file from a source
+     */
+    case DsAddFromUrl(name, desc, target_algo, url, id) => {
+      try {
+        val downloader = new URL(url) #> new File(s"./datasets/$username/$name")
+        downloader.run()
+      } catch {
+        case _: Throwable => println("Got something unexpected")
+      }
+    }
+
+    /**
+     * Add a file directly from the supplied resource
+     */
+    case DsAddDirect(name, desc, target_algo, file, id) => {
+      try {
+        val filepath = Paths.get(s"./datastore/datsets/$username/$name")
+        if (Files.exists(filepath)) throw new Exception("A file with name already exists")
+        else file.moveTo(new File(s"./datastore/datasets/$username/$name"))
+      } catch {
+        case _: Throwable => println("Got something unexpected")
+      }
+    }
+    case DsDelete(name, id) => {
+      val filepath = Paths.get(s"./datastore/datasets/$username/$name")
+      if (!Files.exists(filepath)) {
+        dsm ! RemoveDatasetRecord(username, name)
+        Files.delete(filepath)
+      }
+    }
+    case DsRefresh(name, id) => {
+      try {
+        val url = (dsm ? GiveUserData(username)) match {
+          case x: Future[Any] => Await.result(x, 30 seconds) match {
+            case datasetEntry(_, _, _, _, url: String) => url
+          }
+        }
+        val downloader = new URL(url) #> new File(s"./datastore/datasetes/$username/$name")
+        downloader.run()
+      } catch {
+        case _: Throwable => println("Got something unexpected")
+      }
+    }
 
     case _ => println("Unhandled message")
   }
 
   def receive = {
-    case SubmitDsOpJob(username: String, job: DataSetOp) => handleDsJob(username, job)
+    case (dsm: ActorRef, SubmitDsOpJob(username: String, job: DataSetOp)) => handleDsJob(username, job, dsm)
   }
 
 }
