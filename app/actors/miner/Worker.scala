914a3cf4568a30b27262e32d47c6a8eec6a1c5cf
bassoGeorge <anishgeorgehb@gmail.com>
2015-04-15 13:03:16 +0530

    Wrote templates for Miner and DsOperator

--- app/actors/miner/Worker.scala
+++ app/actors/miner/Worker.scala
@@ -12,7 +12,14 @@ class Worker (mediator: ActorRef) extends Actor {
     /** Do action against every kind of SubmitMineJob here.
       * When finished, send a Log object to the mediator
       * and send a JobStatus back to the sender
+      *
+      * 1. Mine
+      * 2. Send Result to Persistence,
+      * 3. Persistence will reply with write status
+      * 4. Once all of this is finished, send Log(..) message to mediator
+      * 4. And then send JobStatus back to sender
       */
+    case _ => Unit
   }
 
 }
