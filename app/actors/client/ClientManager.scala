06575be7dfaffd1f414b37849bef561989a08d26
bassoGeorge <anishgeorgehb@gmail.com>
2015-04-22 23:54:16 +0530

    Backend Remodelling complete.

--- app/actors/client/ClientManager.scala
+++ app/actors/client/ClientManager.scala
@@ -4,14 +4,16 @@ import actors.application.AppModule
 import actors.mediator.RegisterForReceive
 import akka.actor.ActorRef
 import akka.pattern.ask
+import akka.util.Timeout
+import models.messages.batchProcessing.BatchProcessorMessage
 import models.messages.client._
 import models.messages.logger.{GetLogs, Log}
-import models.messages.persistenceManaging.DataSetEntry
+import models.messages.persistenceManaging.{DataSetEntry, GetUserDataSets, GetUserResults}
 import play.api.libs.json._
 
 import scala.collection.mutable.{Map => mMap}
 import scala.concurrent.Future
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 /**
  * Created by basso on 22/04/15.
@@ -25,21 +27,31 @@ import models.jsonWrites._
 import scala.concurrent.duration._
 
 class ClientManager (val mediator: ActorRef) extends AppModule {
-  mediator ! RegisterForReceive(self, classOf[ClientMgrMessage])
+  mediator ! RegisterForReceive(self, classOf[ClientManagerMessage])
   val clientTable = mMap[String, ActorRef]()
   import context.dispatcher       // Exectution context for Future
-  implicit val timeout = 3 seconds
+  implicit val timeout = Timeout(30 seconds)
 
   def receive = {
     case LogIn(username) =>
       clientTable += ((username, sender))   // Invariant, the user is unique
-      mediator ? GetLogs(username).asInstanceOf[Future[JsValue]].onComplete {
+      mediator ! new LogIn(username) with BatchProcessorMessage     // Notifiy the batch processor that a new user has logged in
+      (mediator ? GetLogs(username)).asInstanceOf[Future[JsValue]].onComplete {
         case Success(logs) => clientTable(username) ! Push(Json.obj("logs" -> logs))
+        case Failure(ex) => moduleError("Couldn't get logs: exception =>"+ex)
       }
-      // TODO: Then the rest: Request for DataSet
+      (mediator ? GetUserDataSets(username)).asInstanceOf[Future[JsValue]].onComplete {
+        case Success(ds) => clientTable(username) ! Push(Json.obj("data-sets" -> ds))
+        case Failure(ex) => moduleError("Couldn't get Data-sets: exception =>"+ex)
+      }
+      (mediator ? GetUserResults(username)).asInstanceOf[Future[JsValue]].onComplete {
+        case Success(ds) => clientTable(username) ! Push(Json.obj("results" -> ds))
+        case Failure(ex) => moduleError("Couldn't get results: exception =>"+ex)
+      }
 
     case LogOut(username) =>
       clientTable -= username
+      mediator ! new LogOut(username) with BatchProcessorMessage
 
     case MessageToClient(user, pushData) =>
       clientTable get user match {
@@ -47,7 +59,7 @@ class ClientManager (val mediator: ActorRef) extends AppModule {
           case lg: Log => act ! Push(Json.obj("log" -> Json.toJson(lg)))
           case ds: DataSetEntry => Push(Json.obj("data-set" -> Json.toJson(ds)))      // TODO: How to update data-set deletions and all that
         }
-        case None => moduleError("Message for ghost client")
+        case None => Unit   // The client is unavailable, so no push
       }
 
   }
