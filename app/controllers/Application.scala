9c416c3199cdd01f66e3f7c0584e979bee52fe5b
bassoGeorge <anishgeorgehb@gmail.com>
2015-04-27 22:59:24 +0530

    Created the web-socket connection function, added more functions to the controllers.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,30 @@
 package controllers
 
+import akka.actor.ActorRef
+import play.api.Play.current
+import play.api.libs.json.JsValue
 import play.api.mvc._
 
-object Application extends Controller {
+object Application extends Controller with Secured {
 
   def index = Action {
     Ok(views.html.index())
+  }
+
+  def connectJs = Action { implicit request =>
+    Ok(javascripts.js.connect(request))
+  }
+
+  // TODO: Conundrum: Need to secure this but how
+  /* Maybe we don't need to secure this, only the when you reach index can you
+     find this url.. but then, will you get the session security??
+   */
+  def connect = WebSocket.acceptWithActor[JsValue, JsValue] {
+    request => out: ActorRef =>
+        username(request) match {
+          case Some(user) => models.Application.clientProps(user, out)
+          case None => actors.client.UnAuthClient.props(out)
+        }
   }
 
   /*
