2470652b4a51a97b161819b9f3d8599f49f90513
bassoGeorge <anishgeorgehb@gmail.com>
2015-05-01 19:37:16 +0530

    going to make heavy changes to controller format

--- app/controllers/TestController.scala
+++ app/controllers/TestController.scala
@@ -4,7 +4,11 @@ package controllers
  * Created by basso on 29/04/15.
  */
 import models.security.loginForm
+import play.api.libs.iteratee.{Enumerator, Iteratee}
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object TestController extends Controller {
 
@@ -16,7 +20,16 @@ object TestController extends Controller {
     Ok(javascripts.js.testConnect(request))
   }
 
-  def connect = TODO      // web socket actor
+  def connect = WebSocket.using[JsValue] { request =>
+    val in = Iteratee.foreach[JsValue](println).map { _ =>
+        println("Disconnected")
+    }
+    //val out = Enumerator[JsValue](Json.obj("test" -> "hello"))
+    val out = Enumerator[JsValue](
+      Json.obj("logs" -> actors.logger.Logger.getLogs("Anish")("./test/resources/logFile_test_orig"))
+    )
+    (in, out)
+  }
 
   def testLoginForm = Action { implicit request =>
     loginForm.bindFromRequest.fold(
