cbfacb5220e14424ec1816491480129d1acdc295
Chetan Bhasin <git@mail.chetanbhasin.com>
2015-05-06 15:04:27 +0530

    Improvement for master mistake

--- public/javascripts/datadisplay.js
+++ public/javascripts/datadisplay.js
@@ -1,63 +1,108 @@
 /**
-* Created by Chetan Bhasin
-* The purpose of this file is to render the charts from the incoming JSON data
-* while automatically detecting the type of the data, what charts are to be displayed
-* and how they must be displayed to the end user.
-*/
-
+ * Created by Chetan Bhasin
+ * The purpose of this file is to render the charts from the incoming JSON data
+ * while automatically detecting the type of the data, what charts are to be displayed
+ * and how they must be displayed to the end user.
+ */
 /**
-* Final function to be called to create a scatter plot
-* on the screen with the given data
-*/
-function genScatterPlot(container, labels, contents) {
+ * Usage guide:
+ * var chartMaker = new dataDisplay(<id of the container>);
+ * chartmaker.makeChart(<incoming_data>);
+ */
+function dataDisplay(containerid) {
 
-    // Make sure that the function is used when the window is loaded properly
-    window.onload() = function() {
-        // Creating the chart
-        var chart = new CanvasJS.Chart(container,
-        {
-            title : {
-                text : labels.title,
-                fontFamily : "arial black",
-                fontColor : "DarkSlateGrey"
-            },
+    var me = this; // Pointer to self
+    /**
+     * Final function to be called to create a scatter plot
+     * on the screen with the given data
+     */
+    function genScatterPlot(container, labels, contents) {
 
-            // Enable the animation for pleasant surprise
-            animationEnabled : true,
+        // Make sure that the function is used when the window is loaded properly
+        window.onload() = function() {
+            // Creating the chart
+            var chart = new CanvasJS.Chart(container, {
+                title: {
+                    text: labels.title,
+                    fontFamily: "arial black",
+                    fontColor: "DarkSlateGrey"
+                },
 
-            axisX : {
-                title : labels.xtitle
-            },
-            axisY : {
-                title : labels.ytitle
-            },
+                // Enable the animation for pleasant surprise
+                animationEnabled: true,
 
-            // Data points with tooltip label all contents in one place
-            data: [{
-                type : "scatter",
-                toolTipContent : labels.tooltip,
-                dataPoints : contents
-            }]
-        });
+                axisX: {
+                    title: labels.xtitle
+                },
+                axisY: {
+                    title: labels.ytitle
+                },
 
-        // Rendering the chart
-        chart.render();
+                // Data points with tooltip label all contents in one place
+                data: [{
+                    type: "scatter",
+                    toolTipContent: labels.tooltip,
+                    dataPoints: contents
+                }]
+            });
+
+            // Rendering the chart
+            chart.render();
+        }
     }
-}
 
-function makeChart(incoming) {
+    /**
+     * Final function to be called to create a bar chart with
+     * lables on X-axis on the screen with given data
+     */
+    function genBarChart(container, labels, contents) {
+        // Make sure that the function is used when the window is loaded properly
+        window.onload = function() {
+            // Creating the chart
+            var chart = new CanvasJS.chart(container, {
+                title: {
+                    text: labels.title,
+                    fontFamily: "arial black",
+                    fontColor: "DarkSlateGrey"
+                },
 
-    // Match the incoming data with all the available algorithms and act accordigley
-    if (incoming.algorithm === "clustering") {
-    // Matching for K-Means clustering algorithm
-    // Genereate the scatter plot
-        genScatterPlot("dataContainer", {
-            title : incoming.title,
-            xtitle : "",
-            ytitle : "",
-            tooltip : "Cluster {name}"
-        },
-        incoming.data.defaults);
+                // Enable the animatino
+                animationEnabled: true,
+
+                data: [
+                    type: "cloumn",
+                    dataPoints: contents
+                ]
+            })
+        }
     }
-}
 
+    /**
+     * Function to finally create the  hart with the incoming data
+     */
+    function makeChart(incoming) {
+
+        // Match the incoming data with all the available algorithms and act accordigley
+        if (incoming.algorithm === "clustering") {
+            // Matching for K-Means clustering algorithm
+            // Genereate the scatter plot
+            me.genScatterPlot(containerid, {
+                    title: incoming.title,
+                    xtitle: "",
+                    ytitle: "",
+                    tooltip: "Cluster {name}"
+                },
+                incoming.data.defaults);
+        } else if (incoming.algorithm === "fpm") {
+            // Matching for FP-Growth algorithm
+            // Generating the bar chart
+            me.genBarChart(containerid, {
+                title: incoming.name,
+                xtitle: "Itemsets",
+                ytitle: "Frequence"
+            }, incoming.data);
+        }
+    }
+
+    return makeChart;
+}
